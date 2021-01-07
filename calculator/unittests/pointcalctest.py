e3915584a7f2ef527a5282a0c178ee7d3acc3ef3
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-01-07 11:43:52 +0100

    Eerste unittests geschreven voor pointcalctest opgezet, kan e.v.t. uitgebreid worden.

--- calculator/unittests/pointcalctest.py
+++ calculator/unittests/pointcalctest.py
@@ -1,3 +1,4 @@
+from ..main import Point
 import unittest
 
 # Onderdeel 1: De simulatie kan op een punt in een gebied de schade uitrekenen.
@@ -7,5 +8,15 @@ class PointCalcTest(unittest.TestCase):
     """Provides info for points, and expects the damage as an output."""
 
     def SinglePoint1(self):
-        """Calculates the damage for a single point."""
-        dmgperml = 3.49
+        """Calculates the damage for a single point, using a constant value."""
+        point = Point(0.53,20)  # $/ml and ml's.
+        self.assertEqual(point.getCost(), 10.60)
+
+    def SinglePoint2(self):
+        """Calculates the damage for a single point, using a simple exponential formula function"""
+        def expcost(x):
+            """Takes the amount of ml in damages and outputs the cost"""
+            return x**2 + 3
+
+        point = Point(expcost,10)
+        self.assertEqual(point.getCost(), 103)
