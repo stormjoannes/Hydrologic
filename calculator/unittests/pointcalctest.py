fd918b81c2b551e6f85adc5574c01d216d46d36d
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-01-11 10:27:25 +0100

    Unittests geschreven voor de calculator, specifiek de Area.

--- calculator/unittests/pointcalctest.py
+++ calculator/unittests/pointcalctest.py
@@ -5,15 +5,20 @@ import unittest
 # De bedoeling is dat dit uiteindelijk op deze manier uitgebreid kan worden voor een geheel gebied.
 
 class PointCalcTest(unittest.TestCase):
-    """Provides info for points, and expects the damage as an output."""
+    """Provides info for points, and expects the damage as an output.
+    Functionality: Calculate damages on each point in a given property.
+    Points are defines as a 1 by 1 meter space in the property, although
+    this can be finetuned."""
 
     def SinglePoint1(self):
-        """Calculates the damage for a single point, using a constant value."""
+        """Calculates the damage for a single point, using a constant value.
+        Functionality: Base damage calculation."""
         point = Point(0.53,20)  # $/ml and ml's.
         self.assertEqual(point.getCost(), 10.60)
 
     def SinglePoint2(self):
-        """Calculates the damage for a single point, using a simple exponential formula function"""
+        """Calculates the damage for a single point, using a simple exponential formula function.
+        Functionality: Damage calculation through supplied function."""
         def expcost(x):
             """Takes the amount of ml in damages and outputs the cost"""
             return x**2 + 3
