206fe96c58b631d31a7e4b449cc4a5fa2ca55c8e
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-01-12 11:50:33 +0100

    Roer omgegooid, we maken de calculator zoals uitgelegd is op schadeschatter.nl. historicaldatatest.py moet nog aan gewerkt worden; het idee is om de calculator te proberen te valideren a.d.h.v. historische data. Momenteel is reductionfactortest.py deels geïmplementeerd; deze werkt volledig voor calculaties in bebouwingsomgevingen.

---
+++ calculator/unittests/historicaldatatest.py
@@ -0,0 +1,2 @@
+# Attempts to test the accuracy of the model by subjecting the output to
+# historical data
---
+++ calculator/unittests/reductionfactortest.py
@@ -0,0 +1,63 @@
+# Doel: Opstellen van reductiefactor-formules voor gebruik in de calculator
+# Er zijn 3 soorten reductiefactoren voor directe schade;
+# - Reductiefactor inundatiediepte
+# - Reductiefactor duur overlast
+# - Reductiefactor seizoen
+# De reductiefactoren kennen een unieke grafiek/formule voor elk classificatie.
+# De volgende classificaties zijn present:
+# - Bebouwing (urban)
+# - Wegen (infrastructuur)
+# - Gewassen (landbouw)
+
+from ..main import Calc
+import unittest
+
+class urbantest(unittest.TestCase):
+    """Tests the reductionfactors for urban damages.
+    Here, the reductionfactors for duration and season are always 1,
+    and should be tested to ensure this is the case. The only varying
+    reductionfactor is the inundationdepth"""
+
+    def test_inundationfactorzero(self):
+        """Tests a case in which the inundation reduction factor should be 0."""
+        calc = Calc(type="URBAN",subtype="URBAN",usage="HIGH",scenario="HIGH",inundepth=-0.01, days=5, month=3)  # Inundationdepth(-0.01) == Factor(0)
+        factor = calc.inunfactor
+        self.assertEqual(factor, 0)
+
+    def test_inundationfactorhalf(self):
+        """Tests a case in which the inundation reduction factor should be 0.5"""
+        calc = Calc(type="URBAN",subtype="URBAN",usage="HIGH",scenario="HIGH",inundepth=0.05, days=5, month=3)  # Inundationdepth(0.05) == Factor(0.5)
+        factor = calc.inunfactor
+        self.assertEqual(factor, 0.5)
+
+    def test_inundationfactorfull(self):
+        """Tests a case in which the inundation reduction factor should be 1"""
+        calc = Calc(type="URBAN",subtype="URBAN",usage="HIGH",scenario="HIGH",inundepth=0.15, days=5, month=3)  # Inundationdepth(0.15) == Factor(1)
+        factor = calc.inunfactor
+        self.assertEqual(factor, 1)
+
+    def test_durationfactorconsistency(self):
+        """Tests whether the duration reduction factor is always consistent (is 1 in different cases)"""
+        for i in range(0, 21):
+            calc = Calc(type="URBAN",subtype="URBAN",usage="HIGH",scenario="HIGH",inundepth=0.3, days=i, month=3)
+            factor = calc.durfactor
+            self.assertEqual(factor, 1)
+
+    def test_seasonfactorconsistency(self):
+        """Tests whether the duration reduction factor is always consistent (is 1 in different cases)"""
+        for j in range(0,13):
+            calc = Calc(type="URBAN",subtype="URBAN",usage="HIGH",scenario="HIGH",inundepth=0.3, days=5, month=j)
+            factor = calc.seasonfactor
+            self.assertEqual(factor, 1)
+
+
+class infrastructuretest(unittest.TestCase):
+    """Tests the reductionfactors for infrastructure damages."""
+
+    def test_factorzero(self):
+        """Tests a case in which the reduction factor should be 0."""
+
+class agriculturaltest(unittest.TestCase):
+    """Tests the reductionfactors for agricultural damages."""
+    def test_factorzero(self):
+        """Tests a case in which the reduction factor should be 0."""
