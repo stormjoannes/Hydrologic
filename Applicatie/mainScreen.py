eff21ba69bb724300bcfa6b88bbc3511fe31a02c
stormjoannes <stormjoannes10@hotmail.com>
2021-01-21 13:07:30 +0100

    beter geformat

--- Applicatie/mainScreen.py
+++ Applicatie/mainScreen.py
@@ -1,40 +1,38 @@
-import base64
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, State, Input
-from buildings import create_data
+from Applicatie.buildings import create_data
 import plotly.graph_objects as go
 
-mapbox_access_token = "pk.eyJ1IjoiY2hhcmxpZWNob2YiLCJhIjoiY2trMmozbzJwMGp1NDJwcW94dHAzdmYxZSJ9.PWhcvXLn2xNSZV_gkKpXbw"
-#Invoeren alle latitudes en longtitudes met bijbehorende gegevens
-fig = go.Figure(go.Scattermapbox(
-        lat=['52.1044958',
-             '52.1047415'],
-        lon=['5.0986251',
-             '5.0984616'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-        ),
-        text=["Thijmes huis",
-              "overkant"],
-    ))
+def makeMap(latitudes, longtitudes, calculations):
+    mapbox_access_token = "pk.eyJ1IjoiY2hhcmxpZWNob2YiLCJhIjoiY2trMmozbzJwMGp1NDJwcW94dHAzdmYxZSJ9.PWhcvXLn2xNSZV_gkKpXbw"
+    #Invoeren alle latitudes en longtitudes met bijbehorende gegevens
+    fig = go.Figure(go.Scattermapbox(
+            lat=latitudes,
+            lon=longtitudes,
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+            ),
+            text=calculations,
+        ))
 
-#Instellingen voor de map en het begin pointview
-fig.update_layout(
-    autosize=True,
-    hovermode='closest',
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        bearing=0,
-        center=dict(
-            lat=52.1064958,
-            lon=5.101
+    #Instellingen voor de map en het begin pointview
+    fig.update_layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken=mapbox_access_token,
+            bearing=0,
+            center=dict(
+                lat=52.1064958,
+                lon=5.101
+            ),
+            pitch=0,
+            zoom=14.5
         ),
-        pitch=0,
-        zoom=14.5
-    ),
-)
+    )
+    return fig
 
 #Import concents
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -70,49 +68,26 @@ if __name__ == '__main__':
                 html.Label('Kies de benodigde files van de buurt',
                            style={'marginLeft': 22.5}),
                 html.Div([
-                    dcc.Upload(
-                        id='upload-image',
-                        children=html.Div([
-                            'Drag and Drop or ',
-                            html.A('Select Files')
-                        ]),
-                        style={'width': '60%',
-                               'padding': 3,
-                               'verticalAlign': 'middle',
-                               'marginLeft': 22,
-                               'marginBottom': 10,
-                               'lineHeight': '60px',
-                               'borderWidth': '1px',
-                               'borderStyle': 'dashed',
-                               'borderRadius': '5px',
-                               'textAlign': 'center',
-                               'background': colors['SubmitButtonBackground']
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True
+                    dcc.Dropdown(
+                        id='gekozen_buurt',
+                        options=[
+                            {'label': 'Ondiep', 'value': 'Ondiep'},
+                            {'label': 'Witte Vrouwen', 'value': 'Witte Vrouwen'}
+                        ],
+                        value='Initial Value',
+                        style={'marginLeft': 11,
+                               'marginBottom': 11,
+                               'width': '50%'}
                     ),
-                    html.Div(id='output-image-upload'),
                 ]),
-                # html.Label('Prijs',
-                #            style={'marginLeft': 22.5}),
-                # dcc.Dropdown(
-                #     options=[
-                #         {'label': 'Boven', 'value': 'boven'},
-                #         {'label': 'Onder', 'value': 'onder'},
-                #     ],
-                #     value='prijs',
-                #     id='reparatieMaanden',
-                #     style={'marginLeft': 11,
-                #            'marginBottom': 11,
-                #            'width':'50%'}),
 
                 html.Label('Scenario',
                            style={'marginLeft': 22.5}),
                 dcc.RadioItems(
                     options=[
-                        {'label': 'Laag', 'value': 'Laag'},
-                        {'label': 'Gemiddeld', 'value': 'Gemiddelde'},
-                        {'label': 'Hoog', 'value': 'Hoog'}
+                        {'label': 'Laag', 'value': 'LOW'},
+                        {'label': 'Gemiddeld', 'value': 'MEDIUM'},
+                        {'label': 'Hoog', 'value': 'HIGH'}
                     ],
                     value='Initial Value',
                     id='scenario',
@@ -138,7 +113,7 @@ if __name__ == '__main__':
                        'backgroundColor': colors['MainBackground']}),
 
             html.Div([
-                dcc.Graph(figure=fig),
+                # dcc.Graph(figure=fig, style={'display':'None'}),
                 html.Div('Berekenen waterschade...',
                          id='output_div',
                          style={'color': 'white',
@@ -158,42 +133,33 @@ if __name__ == '__main__':
     ], style={'columnCount': 1,
               'backgroundColor': colors['MainBackground']})
 
-    def parse_contents(contents, filename):
-        return html.Div([
-            html.H5(filename),
-
-            # HTML images accept base64 encoded strings in the same format
-            # that is supplied by the upload
-            html.Img(src=contents)],
-            style= {'marginLeft':22}
-        )
-
-    @app.callback(Output('output-image-upload', 'children'),
-                  Input('upload-image', 'contents'),
-                  State('upload-image', 'filename'))
-
-    def update_output(list_of_contents, list_of_names):
-        if list_of_contents is not None:
-            children = [
-                parse_contents(c, n) for c, n in
-                zip(list_of_contents, list_of_names)]
-            return children
-
     #verkrijgen van de input values
     @app.callback(Output('output_div', 'children'),
                   Input('submitButton', 'n_clicks'),
-                  State('upload-image', 'contents'),
+                  State('gekozen_buurt', 'value'),
                   State('scenario', 'value'))
 
-    def update_output(clicks, uploadimage, scenario):
-        antwoorden = [uploadimage, scenario]
+    def update_output(clicks, gekozen_buurt, scenario):
+        antwoorden = [gekozen_buurt, scenario]
         #zorgen dat alles ingevuld is voordat je op submit kan drukken en resultaten krijgt
-        if uploadimage != 'None':
-            print(uploadimage)
-            content_type, content_string = uploadimage[0].split(',')
-            decoded = base64.b64decode(content_string)
         if 'Initial Value' not in antwoorden and None not in antwoorden:
-            return create_data(decoded, scenario, ['gebruiksdo', 'oppervlakt', 'MAX'])
+            data = create_data(gekozen_buurt, scenario, ['gebruiksdo', 'oppervlakt', 'MAX', 'LAT', 'LNG'])
+            latitudes = []
+            longtitudes = []
+            calculations = []
+            for i in data:
+                #De latitude min de correcte van de goede coördinaten
+                latitudes.append(float(i.lat) - 0.0009846483658)
+                #De latitude min de correcte van de goede coördinaten
+                longtitudes.append(float(i.lng) - 0.0003943217995)
+                #Format voor de geschatte waterschade
+                calculations.append('geschatte waterschade: €' + str(round(i.waterschatting, 2)))
+            print(latitudes)
+            return dcc.Graph(figure=makeMap(latitudes, longtitudes, calculations),
+                             style={'height':'100%',
+                                    'width':'100%',
+                                    'color': colors['MainBackground']})
         else:
             return 'Vul de nodige gegevens in'
 
+    app.run_server(debug=True)
